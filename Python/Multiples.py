b17bd775b85eebf9956166a00e70daea2b64110c
Robot112233 <30312919+Robot112233@users.noreply.github.com>
2017-07-31 12:47:52 +0300

    Update Multiples.py

--- Python/Multiples.py
+++ Python/Multiples.py
@@ -4,7 +4,7 @@
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 """
 
-#python 3.0 code
+#python 3.6.2 code
 
 def multiples(num):
 	a=0
