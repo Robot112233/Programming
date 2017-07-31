eff94a694f540fcad511616f0ea165bb789049a0
Robot112233 <30312919+Robot112233@users.noreply.github.com>
2017-07-31 12:44:47 +0300

    Update Multiples.py

--- Python/Multiples.py
+++ Python/Multiples.py
@@ -4,6 +4,8 @@
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 """
 
+#python 3.0 code
+
 def multiples(num):
 	a=0
 	taulu = []
