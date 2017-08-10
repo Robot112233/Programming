13aad0956cb9ee77f18b13b3612e1a451cb0405a
Robot112233 <30312919+Robot112233@users.noreply.github.com>
2017-08-10 19:12:24 +0300

    Update Monte_carlo.cu

--- CUDA/Monte_carlo.cu
+++ CUDA/Monte_carlo.cu
@@ -1,58 +1,65 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <cuda.h>
 #include <time.h>
 #include <curand_kernel.h>
 
-#define Trials               1000
-#define BLOCKS               512
-#define THREADS              512
+#define NUMBER               float
+#define precision            "float"
+#define Cycles               1000
+#define Cycles2              512
+#define BLOCKS               8
+#define THREADS              128
 #define Correct_value        187.5
 
+__global__ void gpu_monte_carlo(NUMBER *estimate, curandState *states){
+  unsigned int tid = threadIdx.x + blockDim.x * blockIdx.x;
+  NUMBER ans, ans2 = 0;
+  NUMBER x, y, a, b, c, d;
+  a = c = 0;
+  b = d = 5;
+  curand_init(1234, tid, 0, &states[tid]);
 
-__global__ void gpu_monte_carlo(double *estimate, curandState *states)
-{
-    unsigned int tid = threadIdx.x + blockDim.x * blockIdx.x;
-    double ans = 0;
-    double x, y, a, b, c, d;
-    a = c = 0;
-    b = d = 5;
-    curand_init(1234, tid, 0, &states[tid]);
-
-    for (int i = 0; i < Trials; ++i)
-    {
-        x = (a+(b-a))*curand_uniform(&states[tid]);
-        y = (c+(d-c))*curand_uniform(&states[tid]);
-        ans += (2*x+y);
+  for (long i = 0; i < Cycles2; i++){
+    ans = 0;
+    for (long i = 0; i < Cycles; ++i){
+      x = (a+(b-a))*curand_uniform(&states[tid]);
+      y = (c+(d-c))*curand_uniform(&states[tid]);
+      ans += (2*x+y);
     }
-    estimate[tid] = ans*(((b-a)*(d-c)) / (double) Trials) ;
+    ans2 += ans/(NUMBER)Cycles;
+  }
+  estimate[tid] = ans2*(((b-a)*(d-c)) / (NUMBER) Cycles2) ;
 }
 
-
-int main ()
-{
-    clock_t start, stop;
-    double host[BLOCKS * THREADS];
-    double *dev;
-    curandState *devStates;
-    printf("# of trials per thread = %d, # of blocks = %d, # of threads/block = %d.\n",
-    Trials, BLOCKS, THREADS);
-    start = clock();
-    cudaMalloc((void **) &dev, BLOCKS * THREADS * sizeof(double));
-    cudaMalloc( (void **)&devStates, THREADS * BLOCKS * sizeof(curandState) );
-    gpu_monte_carlo<<<BLOCKS, THREADS>>>(dev, devStates);
-    cudaMemcpy(host, dev, BLOCKS * THREADS * sizeof(double), cudaMemcpyDeviceToHost);
-    double integral_gpu;
-    for (int i = 0; i < BLOCKS * THREADS; ++i)
-    {
-        integral_gpu += host[i];
-    }
-
-    integral_gpu /= (BLOCKS * THREADS);
-    stop = clock();
-    printf("GPU 2*x+y calculated in %f s.\n", (stop-start)/(double)CLOCKS_PER_SEC);
-    printf("CUDA estimate of 2*x+y = %f [error of %f]\n", integral_gpu, integral_gpu - Correct_value);
-
-    return 0;
+int main (){
+  clock_t t[6];
+  NUMBER host[BLOCKS * THREADS];
+  NUMBER *dev;
+  curandState *devStates;
+  printf("# of cycles per thread = %d, # of blocks = %d, # of threads/block = %d.\n",
+  Cycles, BLOCKS, THREADS);
+  t[0] = clock();
+  cudaMalloc((void **) &dev, BLOCKS * THREADS * sizeof(NUMBER));
+  t[1] = clock();
+  cudaMalloc( (void **)&devStates, THREADS * BLOCKS * sizeof(curandState) );
+  t[2] = clock();
+  gpu_monte_carlo<<<BLOCKS, THREADS>>>(dev, devStates);
+  t[3] = clock();
+  cudaMemcpy(host, dev, BLOCKS * THREADS * sizeof(NUMBER), cudaMemcpyDeviceToHost);
+  t[4] = clock();
+  NUMBER integral_gpu;
+  for (long i = 0; i < BLOCKS * THREADS; ++i){
+    integral_gpu += host[i];
+  }
+  t[5] = clock();
+  integral_gpu /= (NUMBER)(BLOCKS * THREADS);
+  printf("Precision %s\n",precision);
+  printf("CPU mem allocation  %f  ms.\n", ((t[1]-t[0])*1000.0)/(double)CLOCKS_PER_SEC);
+  printf("GPU mem allocation  %f  ms.\n", ((t[2]-t[1])*1000.0)/(double)CLOCKS_PER_SEC);
+  printf("GPU Kernel launch  %f  ms.\n", ((t[3]-t[2])*1000.0)/(double)CLOCKS_PER_SEC);
+  printf("GPU calculation + copy from GPU mem to CPU mem  %f  ms.\n", ((t[4]-t[3])*1000.0)/(double)CLOCKS_PER_SEC);
+  printf("CPU list loop  %f  ms.\n", ((t[4]-t[3])*1000.0)/(double)CLOCKS_PER_SEC);
+  printf("CUDA estimate of 2*x+y = %f [error of %f]\n", integral_gpu, integral_gpu - Correct_value);
+  return 0;
 }
