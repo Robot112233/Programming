388e8131f81c8fc765a8b2c5e771821efd315b91
Robot112233 <30312919+Robot112233@users.noreply.github.com>
2017-08-15 12:28:36 +0300

    Update Thermal_conduction.m

--- Octave/Thermal_conduction.m
+++ Octave/Thermal_conduction.m
@@ -1,11 +1,12 @@
 clc;
 clear all;
 %Note to self BUG IN CODE !!! T_Cold Explodes! 
-m_H2O = 500.0; %kg 
-X_Fe = 3.0; %m
-y_Fe = 10.0; %m
+m_H2O = 20.0; %kg 
+X_Fe = 5.0; %m
+y_Fe = 5.0; %m
+z_fe = 5.0; %m
 rho_Fe = 7.87E03; %Kg/m^3
-V_Fe = X_Fe * y_Fe; %m^3
+V_Fe = X_Fe * y_Fe *z_fe; %m^3
 m_Fe  = rho_Fe / V_Fe; %kg
 k_Fe = 80.4; %w/m*k
 A_Fe = X_Fe*y_Fe;
@@ -15,7 +16,7 @@ c_Fe = 470; % J/kg*K
 T_Hot_H2O = (90.0+273.15);
 T_Cold_H2O = (0);
 T_Cold_Fe = (0+273.15);
-dt = 0.1; %s 
+dt = 1; %s 
 
 %Conduction dQ = dt*k*A*(t_h - t_c)/L
 % Heat requirement to change temperature dQ=mcdT
@@ -25,7 +26,7 @@ T_Cold_H2O_matrix = [];
 %starting temps to matrix 
 T_Cold_H2O_matrix = [T_Cold_H2O_matrix, T_Hot_H2O-273.15]; 
 T_Hot_Fe_matrix = [T_Hot_Fe_matrix, T_Cold_Fe-273.15];
-for i = 1:1200
+for i = 0:350
   %Transferred heat capacity
   Q = k_Fe.*A_Fe.*((T_Hot_H2O-T_Cold_Fe)./X_Fe).*dt;
   %Water cooling 
@@ -39,8 +40,11 @@ for i = 1:1200
   T_Cold_Fe = T_Hot_Fe;
   T_Hot_Fe_matrix = [T_Hot_Fe_matrix, T_Hot_Fe-273.15];
 endfor
-figure(1);
 plot(T_Cold_H2O_matrix);
-figure(2);
-plot(T_Hot_Fe_matrix);
-
+hold
+plot(T_Hot_Fe_matrix,'r');
+xlabel('Time (s)')
+ylabel('Temperature (\deg C)')
+title(' 20 Kg 90\deg C water used to heat 63 kg 0\deg C iron through conduction')
+legend("Water","Fe");
+Holdoff
