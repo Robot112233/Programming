58e9568bff4b8cee9647a76cb41524fff8afcc65
Robot112233 <30312919+Robot112233@users.noreply.github.com>
2017-08-15 11:54:07 +0300

    Update Thermal_conduction.m

--- Octave/Thermal_conduction.m
+++ Octave/Thermal_conduction.m
@@ -1,7 +1,7 @@
 clc;
 clear all;
 %Note to self BUG IN CODE !!! T_Cold Explodes! 
-m_H2O = 5000.0; %kg 
+m_H2O = 500.0; %kg 
 X_Fe = 3.0; %m
 y_Fe = 10.0; %m
 rho_Fe = 7.87E03; %Kg/m^3
@@ -13,42 +13,34 @@ c_H2O = 4190.0; % J/kg*K
 c_Fe = 470; % J/kg*K
 %temperatures
 T_Hot_H2O = (90.0+273.15);
-T_Cold_H2O = (0+273.15);
+T_Cold_H2O = (0);
 T_Cold_Fe = (0+273.15);
-T_Hot_Fe = (90+273.15);
-dt = 0.001; %s 
-T_Hot_Fe_start = (0+273.15);
-Q_Fe =  m_Fe*c_Fe * (T_Hot_Fe_start - T_Cold_Fe);
-
+dt = 0.1; %s 
 
 %Conduction dQ = dt*k*A*(t_h - t_c)/L
 % Heat requirement to change temperature dQ=mcdT
 
 T_Hot_Fe_matrix = [];
 T_Cold_H2O_matrix = [];
-%starting parameter 
-T_Cold_H2O_matrix = [T_Cold_H2O_matrix, T_Hot_H2O]; 
-T_Hot_Fe_matrix = [T_Hot_Fe_matrix, T_Hot_Fe_start];
-
-do
-  %Heat decrease in water
-  Q_H2O = m_H2O*c_H2O * (T_Hot_H2O-T_Cold_H2O);
+%starting temps to matrix 
+T_Cold_H2O_matrix = [T_Cold_H2O_matrix, T_Hot_H2O-273.15]; 
+T_Hot_Fe_matrix = [T_Hot_Fe_matrix, T_Cold_Fe-273.15];
+for i = 1:1200
   %Transferred heat capacity
-  Q2 = (k_Fe.*A_Fe.*((T_Hot_Fe-T_Cold_Fe)./X_Fe)).*dt + Q_H2O;
-  %Water cooling due to heat transfer
-  T_Cold_H2O = -((Q2-Q_H2O)./m_H2O.*c_H2O)+T_Hot_H2O; 
+  Q = k_Fe.*A_Fe.*((T_Hot_H2O-T_Cold_Fe)./X_Fe).*dt;
+  %Water cooling 
+  T_Cold_H2O = T_Hot_H2O - (Q./(m_H2O.*c_H2O));
   %Fe temperature increase by thermal coduction
-  T_Cold_Fe = ((Q2-Q_Fe)./m_Fe.*c_Fe)+T_Hot_Fe_start;
-  %Heat incrase Fe
-  Q_Fe =  m_Fe.*c_Fe*(T_Cold_Fe-T_Hot_Fe_start);
+  T_Hot_Fe = (Q./(m_Fe.*c_Fe)) + T_Cold_Fe;
+ 
   %data gathering and value change
-  T_Cold_H2O_matrix = [T_Cold_H2O_matrix, T_Cold_H2O];
+  T_Cold_H2O_matrix = [T_Cold_H2O_matrix, T_Hot_H2O-273.15];
   T_Hot_H2O = T_Cold_H2O;
-  T_Hot_Fe = T_Cold_Fe;
-  T_Hot_Fe_start = T_Cold_Fe;
-  T_Hot_Fe_matrix = [T_Hot_Fe_matrix, T_Hot_Fe_start];
-until (T_Cold_H2O = T_Hot_Fe) 
+  T_Cold_Fe = T_Hot_Fe;
+  T_Hot_Fe_matrix = [T_Hot_Fe_matrix, T_Hot_Fe-273.15];
+endfor
 figure(1);
 plot(T_Cold_H2O_matrix);
 figure(2);
 plot(T_Hot_Fe_matrix);
+
