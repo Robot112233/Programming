92065601f876ae8f3efee77827cdecafd02fd235
Robot112233 <30312919+Robot112233@users.noreply.github.com>
2017-08-19 18:18:58 +0300

    Update Thermal_conduction.m

--- Octave/Thermal_conduction.m
+++ Octave/Thermal_conduction.m
@@ -1,5 +1,8 @@
+more off;
+%close all;
 clc;
 clear all;
+
 m_H2O = 20.0; %kg 
 X_Fe = 0.30; %m
 y_Fe = 0.30; %m
@@ -25,6 +28,8 @@ T_Cold_H2O_matrix = [];
 %starting temps to matrix 
 T_Cold_H2O_matrix = [T_Cold_H2O_matrix, T_Hot_H2O-273.15]; 
 T_Hot_Fe_matrix = [T_Hot_Fe_matrix, T_Cold_Fe-273.15];
+tValues=[0.0];
+t=0;
 for i = 0:200
   %Transferred heat capacity
   Q = k_Fe.*A_Fe.*((T_Hot_H2O-T_Cold_Fe)./X_Fe).*dt;
@@ -38,12 +43,18 @@ for i = 0:200
   T_Hot_H2O = T_Cold_H2O;
   T_Cold_Fe = T_Hot_Fe;
   T_Hot_Fe_matrix = [T_Hot_Fe_matrix, T_Hot_Fe-273.15];
+  t=t+dt;
+  tValues = [tValues, t];
 endfor
-plot(T_Cold_H2O_matrix);
-hold
-plot(T_Hot_Fe_matrix,'r');
-xlabel('Time (min)')
-ylabel('Temperature (\deg C)')
-title(' 20 Kg 90\deg C water used to heat 212.5 kg 0\deg C iron through conduction')
-legend("Water","Fe");
-Holdoff
+plot(tValues, T_Cold_H2O_matrix, 'b-', 'linewidth', 2);
+set(gca, "linewidth", 1, "fontsize", 17); 
+hold on
+plot(tValues, T_Hot_Fe_matrix,'r-', 'linewidth', 2);
+xlabel('Time (s)','fontsize', 18);
+ylabel('Temperature (\deg C)','fontsize', 18);
+title(' 20 Kg 90\deg C water used to heat 212.5 kg 0\deg C iron through conduction','fontsize', 16)
+a = legend("Water","Fe");
+set(a,"fontsize", 20);
+xlim([0, max(tValues)]);
+hold off
+grid on
